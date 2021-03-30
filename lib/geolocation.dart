959124e3ce189e067b1f444a68cac9ef88301ea1
Yannick Granger <ygranger@mediproductions.com>
2021-03-30 17:13:54 +0200

    add static method to get distance between 2 points

--- lib/geolocation.dart
+++ lib/geolocation.dart
@@ -219,6 +219,35 @@ class Geolocation {
   static bool loggingEnabled = false;
 
   static final _LocationChannel _locationChannel = new _LocationChannel();
+
+  /// Calculates the distance between the supplied coordinates in meters.
+  ///
+  /// The distance between the coordinates is calculated using the Haversine
+  /// formula (see https://en.wikipedia.org/wiki/Haversine_formula). The
+  /// supplied coordinates [startLatitude], [startLongitude], [endLatitude] and
+  /// [endLongitude] should be supplied in degrees.
+  static double distanceBetween(
+      double startLatitude,
+      double startLongitude,
+      double endLatitude,
+      double endLongitude,
+      ) {
+    var earthRadius = 6378137.0;
+    var dLat = _toRadians(endLatitude - startLatitude);
+    var dLon = _toRadians(endLongitude - startLongitude);
+
+    var a = math.pow(math.sin(dLat / 2), 2) +
+        math.pow(math.sin(dLon / 2), 2) *
+            math.cos(_toRadians(startLatitude)) *
+            math.cos(_toRadians(endLatitude));
+    var c = 2 * math.asin(math.sqrt(a));
+
+    return earthRadius * c;
+  }
+
+  static _toRadians(double degree) {
+    return degree * math.pi / 180;
+  }
 }
 
 class GeolocationException implements Exception {
